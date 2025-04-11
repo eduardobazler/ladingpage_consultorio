ee0f0c6f750b61f69f608bcb136192cc766e5f4b
eduardo bazler <edubazler@gmail.com>
2025-04-11 12:44:24 -0300

    teste imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import Image from 'next/image';
+
+
 function Home() {
-    return <div>Dra Vitória Santos</div>
+    return <div>Dra Vitória Santos
+        <Image 
+        src="/files/LOGOTIPO.png"
+        alt="logo consult[orio"
+        width={300}
+        height={400}
+        style={{ borderRadius: '1rem' }}
+        />
+
+    </div>
 }
 
 export default Home;
